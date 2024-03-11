4f21630e6108bc012fc9e1fa4a47cf427fc2ed8c
Eric <ericacccls@gmail.com>
2024-03-11 16:45:55 +0800

    successfully setup vscode opencv env

--- Homework1/src/main.cpp
+++ Homework1/src/main.cpp
@@ -1,24 +1,12 @@
-#define cimg_use_jpeg
-#define JPEG_LIB_VERSION 80
-#include "jconfig.h"
-#include "CImg.h"
-using namespace cimg_library;
- 
-// to compile: g++ -O3 -o try main.cpp -lm -lpthread -lX11
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-int main() {
-  CImg<unsigned char> image("./asset/SceneFromCamera1.jpg"), visu(1080, 1920, 1,3,0);
-  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
-  image.blur(2.5);
-  CImgDisplay main_disp(image,"Click a point"), draw_disp(visu,"Intensity profile");
-  while (!main_disp.is_closed() && !draw_disp.is_closed()) {
-    main_disp.wait();
-    if (main_disp.button() && main_disp.mouse_y()>=0) {
-      const int y = main_disp.mouse_y();
-      visu.fill(0).draw_graph(image.get_crop(0,y,0,0,image.width()-1,y,0,0),red,1,1,0,255,0);
-      visu.draw_graph(image.get_crop(0,y,0,1,image.width()-1,y,0,1),green,1,1,0,255,0);
-      visu.draw_graph(image.get_crop(0,y,0,2,image.width()-1,y,0,2),blue,1,1,0,255,0).display(draw_disp);
-      }
-    }
+int main()
+{
+  cv::Mat image = cv::imread("asset\\SceneFromCamera1.jpg");
+  while(cv::waitKey(0) != 69)
+  {
+    cv::imshow("image", image);
+  }
   return 0;
 }
