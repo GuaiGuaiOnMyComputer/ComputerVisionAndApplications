e28a6060064d8abe8ac606f26e04af3fe2227f67
Eric <ericacccls@gmail.com>
2024-03-27 19:17:02 +0800

    Created mask of the cat paint area and the lady paint area.
    cv assert fails on ```cv::perspectiveTransform```
    
    creating mask for the paint fail
    
    specified compiler path
    
    new method ```floatPointToIntPoint```
    
    successfully filled the mask area by self-implemented function

--- Homework2/src/coordinateparser.cpp
+++ Homework2/src/coordinateparser.cpp
@@ -14,11 +14,11 @@ namespace hw2
     class xyparser
     {
     public: 
-        static std::vector<cv::Point2i> GetCoordinateFromFile(const fs::path& filePath)
+        static std::vector<cv::Point2f> GetCoordinateFromFile(const fs::path& filePath)
         {
             std::ifstream fileHandle(filePath, std::ifstream::in);
             uint32_t pointCount = _getLineCount(fileHandle);
-            std::vector<cv::Point2i> parsedCoordinates;
+            std::vector<cv::Point2f> parsedCoordinates;
             parsedCoordinates.reserve(pointCount);
 
             std::string lineContent;
@@ -35,19 +35,19 @@ namespace hw2
         static uint32_t _getLineCount(std::ifstream& fileHandle)
         {
             fileHandle.seekg(0, std::ios_base::beg);
-            uint32_t lineCount = std::count(std::istreambuf_iterator<char>(fileHandle), std::istreambuf_iterator<char>(), '\n');
+            uint32_t lineCount = 1 + std::count(std::istreambuf_iterator<char>(fileHandle), std::istreambuf_iterator<char>(), '\n');
             fileHandle.seekg(0, std::ios_base::beg);
             return lineCount;
         }
 
-        static cv::Point2i _parseLineAsPoint(const std::string& lineContent)
+        static cv::Point2f _parseLineAsPoint(const std::string& lineContent)
         {
             size_t intStart, intEnd;
             int32_t x, y;
             const char *stringEndPtr = lineContent.data() + lineContent.size();
             std::from_chars_result parsedResult = std::from_chars(lineContent.data(), stringEndPtr, x);
             std::from_chars(parsedResult.ptr + 1, stringEndPtr, y); // +1 to skip the \t delimiter
-            return cv::Point2i(x, y);
+            return cv::Point2f(x, y);
         }
 
         xyparser() = delete;
--- Homework2/src/main.cpp
+++ Homework2/src/main.cpp
@@ -3,19 +3,27 @@
 #include <algorithm>
 #include <vector>
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
 #include "coordinateparser.cpp"
 
 #define MAIN wWinMain
 
 #ifdef _DEBUG
 template class std::vector<cv::Point2i>;
+template class std::vector<cv::Point2f>;
+template class std::array<cv::Point, 4>;
+template class std::vector<std::vector<cv::Point>>;
 #endif
 
 namespace fs = std::filesystem;
 
 bool checkFilesExist(const std::vector<fs::path> filePaths);
-void catToLadyAndLadyToCat(cv::Mat galleryImage, const std::vector<cv::Point2i>& catCornerPoints, const std::vector<cv::Point2i>& ladyCornerPoints);
+void pleaseMakeSureThePolygonBoundedByTheseVerticsIsAboslutelyFilled(cv::InputOutputArray image, std::vector<cv::Point> &vertics);
+cv::Mat catToLadyAndLadyToCat(cv::Mat galleryImage, const std::vector<cv::Point2f>& catCornerPoints, const std::vector<cv::Point2f>& ladyCornerPoints);
+std::vector<cv::Point> floatPointToIntPoint(const std::vector<cv::Point2f> &vtrOfFloatPoints);
 
 int MAIN(int argc, char** agrv)
 {
@@ -31,12 +39,14 @@ int MAIN(int argc, char** agrv)
         })
     );
 
-    std::vector<cv::Point2i> ladyPaintCornerCoordinate = hw2::xyparser::GetCoordinateFromFile(LADY_COOR_FILE_PATH);
-    std::vector<cv::Point2i> catPaintCornerCoordinate = hw2::xyparser::GetCoordinateFromFile(CAT_COOR_FILE_PATH);
+    std::vector<cv::Point2f> ladyPaintCornerCoordinate = hw2::xyparser::GetCoordinateFromFile(LADY_COOR_FILE_PATH);
+    std::vector<cv::Point2f> catPaintCornerCoordinate = hw2::xyparser::GetCoordinateFromFile(CAT_COOR_FILE_PATH);
 
     cv::Mat galleryImage = cv::imread(ART_GALLERY_FILE_PATH.string(), cv::ImreadModes::IMREAD_COLOR);
-    catToLadyAndLadyToCat(galleryImage, catPaintCornerCoordinate, ladyPaintCornerCoordinate);
+    cv::Mat catAndLadyFaceSwapped = catToLadyAndLadyToCat(galleryImage, catPaintCornerCoordinate, ladyPaintCornerCoordinate);
 
+    cv::imshow("Lady is cat and cat is lady", catAndLadyFaceSwapped);
+    cv::waitKey(0);
     return 0;
 }
 
@@ -45,7 +55,55 @@ bool checkFilesExist(const std::vector<fs::path> filePaths)
     return std::all_of(filePaths.cbegin(), filePaths.cend(), [](const fs::path &fp){ return fs::exists(fp); });
 }
 
-void catToLadyAndLadyToCat(cv::Mat galleryImage, const std::vector<cv::Point2i>& catCornerPoints, const std::vector<cv::Point2i>& ladyCornerPoints)
+cv::Mat catToLadyAndLadyToCat(cv::Mat galleryImage, const std::vector<cv::Point2f>& catCornerPoints, const std::vector<cv::Point2f>& ladyCornerPoints)
 {
+    cv::Mat outputImageWithCatAndLadySwapped = galleryImage.clone();
+    cv::Mat galleryImageFloat = cv::Mat();
 
+    std::vector<cv::Point> catMaskVerticsInt = floatPointToIntPoint(catCornerPoints);
+    std::vector<cv::Point> ladyMaskVerticsInt = floatPointToIntPoint(ladyCornerPoints);
+
+    cv::Mat catAreaMask(galleryImage.size(), CV_8UC1);
+    cv::Mat ladyAreaMask(galleryImage.size(), CV_8UC1);
+    pleaseMakeSureThePolygonBoundedByTheseVerticsIsAboslutelyFilled(catAreaMask, catMaskVerticsInt);
+    pleaseMakeSureThePolygonBoundedByTheseVerticsIsAboslutelyFilled(ladyAreaMask, ladyMaskVerticsInt);
+
+    cv::imshow("Cat area mask", catAreaMask);
+    cv::imshow("Lady area mask", ladyAreaMask);
+    cv::waitKey(0);
+
+    cv::Mat tmp;
+    cv::Mat catToLadyHomographyMat = cv::findHomography(catCornerPoints, ladyCornerPoints, 0);
+    cv::Mat catOnLady = cv::Mat::zeros(galleryImageFloat.size(), galleryImageFloat.type());
+    cv::warpPerspective(galleryImage, tmp, catToLadyHomographyMat, galleryImage.size());
+    cv::copyTo(tmp, outputImageWithCatAndLadySwapped, ladyAreaMask);
+
+    cv::Mat ladyToCatHomographyMat = cv::findHomography(ladyCornerPoints, catCornerPoints, 0);
+    cv::Mat ladyOnCat = cv::Mat::zeros(galleryImageFloat.size(), galleryImageFloat.type());
+    cv::warpPerspective(ladyCornerPoints, tmp, ladyToCatHomographyMat, galleryImage.size());
+    cv::copyTo(tmp, outputImageWithCatAndLadySwapped, catAreaMask);
+
+    return outputImageWithCatAndLadySwapped;
+}
+
+std::vector<cv::Point> floatPointToIntPoint(const std::vector<cv::Point2f>& vtrOfFloatPoints)
+{
+    std::vector<cv::Point> intPoints;
+    intPoints.reserve(vtrOfFloatPoints.size());
+    for(const cv::Point2f& ptFloat : vtrOfFloatPoints)
+        intPoints.emplace_back((int)ptFloat.x, (int)ptFloat.y);
+    return intPoints;
+}
+
+void pleaseMakeSureThePolygonBoundedByTheseVerticsIsAboslutelyFilled(cv::InputOutputArray image, std::vector<cv::Point>& vertics)
+{
+    std::vector<cv::Point> triangleVertics(3);
+    std::array<uint32_t, 4> indics{0, 1, 2, 3};
+    while (std::next_permutation(indics.begin(), indics.end()))
+    {
+        triangleVertics.at(0) = vertics[indics[0]];
+        triangleVertics.at(1) = vertics[indics[1]];
+        triangleVertics.at(2) = vertics[indics[2]];
+        cv::fillConvexPoly(image, triangleVertics, cv::Scalar(255));
+    }
 }
