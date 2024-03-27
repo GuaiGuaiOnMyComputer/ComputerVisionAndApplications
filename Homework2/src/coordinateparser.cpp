16ff5aae2e9345eb3bb08b2da74b2cb5465cec65
Eric <ericacccls@gmail.com>
2024-03-27 16:14:16 +0800

    '''-municode``` must be passed and define main function as wWinMain using the new compiler to avoid "undefined reference to wWinMain error"
    must add option ```-municode``` when compiling to avoid "undefined reference to wWinMain" error
    
    comfirmed ```xyparser``` works properly

--- Homework2/src/coordinateparser.cpp
+++ Homework2/src/coordinateparser.cpp
@@ -14,11 +14,12 @@ namespace hw2
     class xyparser
     {
     public: 
-        std::vector<cv::Point2i> GetCoordinateFromFile(fs::path filePath)
+        static std::vector<cv::Point2i> GetCoordinateFromFile(const fs::path& filePath)
         {
             std::ifstream fileHandle(filePath, std::ifstream::in);
             uint32_t pointCount = _getLineCount(fileHandle);
-            std::vector<cv::Point2i> parsedCoordinates(pointCount);
+            std::vector<cv::Point2i> parsedCoordinates;
+            parsedCoordinates.reserve(pointCount);
 
             std::string lineContent;
             for (size_t i = 0; i < pointCount; i++)
@@ -26,11 +27,12 @@ namespace hw2
                 std::getline(fileHandle, lineContent);
                 parsedCoordinates.push_back(_parseLineAsPoint(lineContent));
             }
+            return parsedCoordinates;
         }
 
     private:
 
-        uint32_t _getLineCount(std::ifstream& fileHandle)
+        static uint32_t _getLineCount(std::ifstream& fileHandle)
         {
             fileHandle.seekg(0, std::ios_base::beg);
             uint32_t lineCount = std::count(std::istreambuf_iterator<char>(fileHandle), std::istreambuf_iterator<char>(), '\n');
@@ -38,13 +40,13 @@ namespace hw2
             return lineCount;
         }
 
-        cv::Point2i _parseLineAsPoint(const std::string& lineContent)
+        static cv::Point2i _parseLineAsPoint(const std::string& lineContent)
         {
             size_t intStart, intEnd;
             int32_t x, y;
             const char *stringEndPtr = lineContent.data() + lineContent.size();
             std::from_chars_result parsedResult = std::from_chars(lineContent.data(), stringEndPtr, x);
-            std::from_chars(parsedResult.ptr, stringEndPtr, y);
+            std::from_chars(parsedResult.ptr + 1, stringEndPtr, y); // +1 to skip the \t delimiter
             return cv::Point2i(x, y);
         }
 
--- Homework2/src/main.cpp
+++ Homework2/src/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <filesystem>
+#include <algorithm>
+#include <vector>
 #include <opencv2/core.hpp>
+#include "coordinateparser.cpp"
 
-int main(int argc, char** agrv)
+#define MAIN wWinMain
+
+#ifdef _DEBUG
+template class std::vector<cv::Point2i>;
+#endif
+
+namespace fs = std::filesystem;
+
+bool checkFilesExist(const std::vector<fs::path> filePaths) 
 {
-
+    return std::all_of(filePaths.cbegin(), filePaths.cend(), [](const fs::path &fp){ return fs::exists(fp); });
 }
+
+int MAIN(int argc, char** agrv)
+{
+    const fs::path LADY_COOR_FILE_PATH = fs::path("asset/lady_paint_corners.xyz");
+    const fs::path CAT_COOR_FILE_PATH = fs::path("asset/cat_paint_corners.xyz");
+    const fs::path ART_GALLERY_FILE_PATH = fs::path("asset/Swap_ArtGallery.jpg");
+    assert(
+        checkFilesExist(
+        {
+            LADY_COOR_FILE_PATH,
+            CAT_COOR_FILE_PATH,
+            ART_GALLERY_FILE_PATH
+        })
+    );
+
+    std::vector<cv::Point2i> ladyPaintCornerCoordinate = hw2::xyparser::GetCoordinateFromFile(LADY_COOR_FILE_PATH);
+    std::vector<cv::Point2i> catPaintCornerCoordinate = hw2::xyparser::GetCoordinateFromFile(CAT_COOR_FILE_PATH);
+    return 0;
+}
+
