f3f998dc7a1e8d69fdf5e81bf4670d49c4ea9809
Eric <ericacccls@gmail.com>
2024-04-02 15:57:36 +0800

    new build tasks for cmake projects

--- MidtermProject/src/main.cpp
+++ MidtermProject/src/main.cpp
@@ -1,11 +1,10 @@
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
 #include <stdint.h>
 
 int main(int32_t argc, char** argv)
 {
     cv::Mat img = cv::imread("assets/ShadowStrip/0000.jpg");
     cv::imshow("Test image", img);
-    cv::waitKey(0);
+    cv::waitKey();
     return 0;
 }
