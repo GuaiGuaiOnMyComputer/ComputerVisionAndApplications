30e0d109015f36fcb270f8ed7bfa18379f5176c3
Eric <ericacccls@gmail.com>
2024-04-06 11:38:16 +0800

    it seems trying to find the camera intrinsic and extrinsic parameters is not viable

---
+++ MidtermProject/src/sculptureslice/sculptureslice.cpp
@@ -0,0 +1,32 @@
+#include <opencv2/opencv.hpp>
+#include <filesystem>
+#include "redpixelfinder.hpp"
+
+namespace 
+{
+    namespace fs = std::filesystem;
+
+    class SculptureSlice
+    {
+    public:
+        SculptureSlice(const fs::path& scanProfileImagePath): _imgPath{scanProfileImagePath}
+        {
+            _redPixelCoordinates.reserve(200);
+        }
+
+        void get_red_scan_line()
+        {
+            cv::Mat scanProfileImage = cv::imread(_imgPath.string());
+            midproj::find_red_pixels(scanProfileImage, _redPixelCoordinates);
+        }
+
+        // dummy constructor
+        SculptureSlice()
+        {
+        }
+    private:
+        std::vector<cv::Point2i> _redPixelCoordinates;
+        const cv::Mat _redProfile;
+        const fs::path _imgPath;
+    };
+} // namespace SculptureSlice
