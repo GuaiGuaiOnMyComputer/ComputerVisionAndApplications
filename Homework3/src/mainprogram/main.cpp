902b0d6f8dbf3a7b5e632732c37a2b8f5be03107
Eric <ericacccls@gmail.com>
2024-04-24 16:08:20 +0800

    added measured 2D and 3D point coordinates

--- Homework3/src/mainprogram/main.cpp
+++ Homework3/src/mainprogram/main.cpp
@@ -9,8 +9,11 @@ int main(int , char**)
     using namespace hw3;
     const std::filesystem::path SANTA_XYZ_PATH("assets/Santa.xyz");
     const std::filesystem::path SANTA_JPG_PATH("assets/Santa.jpg");
+    const std::filesystem::path SANTA_FEATURE_POINTS_2D_XYZ_PATH("assets/SantaFeaturePoints2D.xyz");
+    const std::filesystem::path SANTA_FEATURE_POINTS_3D_XYZ_PATH("assets/SantaFeaturePoints3D.xyz");
 
-    AssetCheck::check_if_asset_exist({SANTA_XYZ_PATH, SANTA_JPG_PATH});
-    std::vector<XyzIo::PointCoorAndNormal> vtr = XyzIo::load_points_from_file<XyzIo::PointCoorAndNormal>(std::filesystem::path("assets//Santa.xyz"), 1);
+    AssetCheck::check_if_asset_exist(
+        {SANTA_XYZ_PATH, SANTA_JPG_PATH, SANTA_FEATURE_POINTS_2D_XYZ_PATH, SANTA_FEATURE_POINTS_3D_XYZ_PATH});
+    std::vector<XyzIo::PointCoorAndNormal> vtr = XyzIo::load_points_from_file<XyzIo::PointCoorAndNormal>(SANTA_XYZ_PATH, 1);
     return 0;
 }
