05b5cae44cf1918a037ee3dbe4dab125a41df2cc
Eric <ericacccls@gmail.com>
2024-05-15 14:50:39 +0800

    moved ```Panel``` enum into ```CameraCalibration``` class

--- Homework4/src/cameracalibration/cameracalibration.hpp
+++ Homework4/src/cameracalibration/cameracalibration.hpp
@@ -7,6 +7,13 @@ namespace hw4
     class CameraCalibration
     {
     public:
+        enum Panel
+        {
+            LEFT = 0,
+            RIGHT,
+            TOP,
+            PANEL_COUNT
+        };
         static cv::Mat find_panel_homography(const std::vector<XyzIo::Coor2D_f>& imagePoints, const std::vector<XyzIo::Coor2D_f>& objectPoints);
         static std::array<cv::Mat, 3> find_panel_homography(const std::array<std::vector<XyzIo::Coor2D_f>, 3> &imagePoints, const std::array<std::vector<XyzIo::Coor2D_f>, 3> &objectPoints);
     };
--- Homework4/src/main/main.cpp
+++ Homework4/src/main/main.cpp
@@ -4,13 +4,6 @@
 #include "assetcheck.hpp"
 #include "cameracalibration.hpp"
 
-enum PANEL
-{
-    LEFT = 0,
-    RIGHT,
-    TOP,
-    PANEL_COUNT
-};
 
 template class std::vector<hw4::XyzIo::Coor2D_f>;
 template class std::vector<hw4::XyzIo::Coor3D_f>;
@@ -18,12 +11,12 @@ template class std::vector<hw4::XyzIo::Coor3D_f>;
 int main(int, char**)
 {
     namespace fs = std::filesystem;
-    const std::array<fs::path, PANEL::PANEL_COUNT> PANEL_CORNERS_PATHS_2D{
+    const std::array<fs::path, hw4::CameraCalibration::Panel::PANEL_COUNT> PANEL_CORNERS_PATHS_2D{
         fs::path("../assets/leftPanelCorners2D.xyz"),
         fs::path("../assets/rightPanelCorners2D.xyz"),
         fs::path("../assets/topPanelCorners2D.xyz"),
     };
-    const std::array<fs::path, PANEL::PANEL_COUNT> PANEL_CORNERS_PATHS_3D{
+    const std::array<fs::path, hw4::CameraCalibration::Panel::PANEL_COUNT> PANEL_CORNERS_PATHS_3D{
         fs::path("../assets/leftPanelCorners3D.xyz"),
         fs::path("../assets/rightPanelCorners3D.xyz"),
         fs::path("../assets/topPanelCorners3D.xyz"),
@@ -31,9 +24,9 @@ int main(int, char**)
     hw4::AssetCheck::check_if_asset_exist(PANEL_CORNERS_PATHS_2D);
     hw4::AssetCheck::check_if_asset_exist(PANEL_CORNERS_PATHS_3D);
 
-    std::array<std::vector<hw4::XyzIo::Coor2D_f>, 3> panelCornerCoors2d;
-    std::array<std::vector<hw4::XyzIo::Coor3D_f>, 3> panelCornerCoors3d;
-    for (size_t i = 0; i < PANEL::PANEL_COUNT;i ++)
+    std::array<std::vector<hw4::XyzIo::Coor2D_f>, hw4::CameraCalibration::Panel::PANEL_COUNT> panelCornerCoors2d;
+    std::array<std::vector<hw4::XyzIo::Coor3D_f>, hw4::CameraCalibration::Panel::PANEL_COUNT> panelCornerCoors3d;
+    for (size_t i = 0; i < hw4::CameraCalibration::Panel::PANEL_COUNT;i ++)
     {
         panelCornerCoors2d[i] = hw4::XyzIo::load_points_from_file<hw4::XyzIo::Coor2D_f, float>(PANEL_CORNERS_PATHS_2D[i], 0);
         panelCornerCoors3d[i] = hw4::XyzIo::load_points_from_file<hw4::XyzIo::Coor3D_f, float>(PANEL_CORNERS_PATHS_3D[i], 0);
